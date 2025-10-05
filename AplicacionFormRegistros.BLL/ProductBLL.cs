f887ce9c6ecc1986dda3aab2b0a3f11c97aa7bb0
Deiiivy <deivypr28@gmail.com>
2025-10-05 11:19:18 -0500

    fix: se actualiza BLL de Person

--- AplicacionFormRegistros.BLL/ProductBLL.cs
+++ AplicacionFormRegistros.BLL/ProductBLL.cs
@@ -6,30 +6,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AplicacionFormRegistros.BLL
 {
     public class ProductBLL
     {
-        private ProductDAL productDAL = new ProductDAL();
+        private readonly ProductDAL _productDAL = new ProductDAL();
 
         public List<Product> GetAll()
         {
-            return productDAL.GetAll();
+            return _productDAL.GetAll();
         }
 
         public void Insert(Product product)
         {
-            productDAL.Insert(product);
+            if (string.IsNullOrWhiteSpace(product.ProductName))
+                throw new Exception("El nombre del producto es obligatorio.");
+
+            if (product.Price <= 0)
+                throw new Exception("El precio debe ser mayor que cero.");
+
+            _productDAL.Insert(product);
         }
 
         public void Update(Product product)
         {
-            productDAL.Update(product);
+            if (product.ProductID <= 0)
+                throw new Exception("Debe seleccionar un producto válido.");
+
+            if (string.IsNullOrWhiteSpace(product.ProductName))
+                throw new Exception("El nombre del producto es obligatorio.");
+
+            if (product.Price <= 0)
+                throw new Exception("El precio debe ser mayor que cero.");
+
+            _productDAL.Update(product);
         }
 
         public void Delete(int id)
         {
-            productDAL.Delete(id);
+            if (id <= 0)
+                throw new Exception("Debe seleccionar un producto válido para eliminar.");
+
+            _productDAL.Delete(id);
         }
     }
 }
